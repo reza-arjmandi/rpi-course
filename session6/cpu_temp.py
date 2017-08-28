a94c101481dc293a12f8305a7cca1d0f73f9e26f
arjmandi <arjmandi.re@gmail.com>
2017-08-28 19:02:30 +0000

    update cpu_temp.py comments

--- session6/cpu_temp.py
+++ session6/cpu_temp.py
@@ -1,8 +1,17 @@
+######################################################################
+#	cpu_temp.py
+#
+# This program measure Raspberry pi cpu temprature and print it 
+# every one second
+#####################################################################
+
 import os, time
 
+cmd='/opt/vc/bin/vcgencmd measure_temp'
+
 while True:
-    dev = os.popen('/opt/vc/bin/vcgencmd measure_temp')
-    cpu_temp_s = dev.read()[5:-3]
-    cpu_temp=float(cpu_temp_s)
-    print(cpu_temp)
+    dev = os.popen(cmd)
+    cpuTempStr = dev.read()[5:-3]
+    cpuTemp=float(cpuTempStr)
+    print(cpuTemp)
     time.sleep(1)
