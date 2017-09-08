d4354f50bb37bb6b49de0038290eeffac1bfe26d
arjmandi <arjmandi.re@gmail.com>
2017-09-08 15:16:44 +0000

    Count_Coins.py updated

--- session8/Count_Coins/Count_Coins.py
+++ session8/Count_Coins/Count_Coins.py
@@ -5,7 +5,12 @@ import sys
 import CaptureDevice
 
 def CountCoins(img, cimg):
-    circles = cv2.HoughCircles(img, cv.CV_HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0)
+    circles = None
+    try:
+        circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
+                            param1=50,param2=30,minRadius=0,maxRadius=0)
+    except:
+        circles = cv2.HoughCircles(img, cv.CV_HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0)
     circles = np.uint16(np.around(circles))
     for i in circles[0, :]:
         # draw the outer circle
