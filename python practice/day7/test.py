9b859353ae92eeaa26265c2538401ba4b5e3139a
FSZolfaghari <z.fa95@yahoo.com>
2017-11-28 17:08:42 +0330

    solution and test of day 7

--- python practice/day7/test.py
+++ python practice/day7/test.py
@@ -1,59 +1,75 @@
 import unittest
 from subprocess import Popen, PIPE, STDOUT
-import string
 import random
 
-def string_generator(size=6, chars=string.ascii_uppercase + string.digits + " " + string.punctuation):
-    return ''.join(random.choice(chars) for _ in range(size))
-		
-def TestBase():
-    stdIn = string_generator()
-    p = Popen(['python', 'solution.py'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)   
-    stdout = p.communicate(input=bytes(stdIn, encoding='utf-8'))[0]
-    expectedStdout = "Hello word\r\n" + stdIn + "\r\n"
-    return (stdout.decode(), expectedStdout)
-	
-class test(unittest.TestCase): 
+
+def int_generator(min=0, max=1000):
+    return random.randint(min, max)
+
+
+def TestBase():      
+    N = int_generator()
+    ourIn = ''
+    ourOut = ''
+    numberInput = []
     
+    for i in range(N):
+        numberInput.insert(i,int_generator())    
+        ourIn += str(numberInput[i]) + " "
+
+    for i in range(N):
+        ourOut += str(numberInput[N-i-1]) + " "
+                
+        
+    stdIn = str(N) + "\r\n" + ourIn + "\r\n"
+    p = Popen(['python', 'solution.py'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+    stdout = p.communicate(input=bytes(stdIn, encoding='utf-8'))[0]
+
+    expectedStdout = ourOut
+    return (stdout.decode(), expectedStdout)
+
+
+class test(unittest.TestCase):
     def testCase1(self):
         result = TestBase()
-        self.assertEqual( result[0], result[1])
-		
+        self.assertEqual(result[0], result[1])
+
     def testCase2(self):
         result = TestBase()
-        self.assertEqual( result[0], result[1])
-		
+        self.assertEqual(result[0], result[1])
+
     def testCase3(self):
         result = TestBase()
-        self.assertEqual( result[0], result[1])
-		
+        self.assertEqual(result[0], result[1])
+
     def testCase4(self):
         result = TestBase()
-        self.assertEqual( result[0], result[1])
-		
+        self.assertEqual(result[0], result[1])
+
     def testCase5(self):
         result = TestBase()
-        self.assertEqual( result[0], result[1])
-		
+        self.assertEqual(result[0], result[1])
+
     def testCase6(self):
         result = TestBase()
-        self.assertEqual( result[0], result[1])
-		
+        self.assertEqual(result[0], result[1])
+
     def testCase7(self):
         result = TestBase()
-        self.assertEqual( result[0], result[1])
-	
+        self.assertEqual(result[0], result[1])
+
     def testCase8(self):
         result = TestBase()
-        self.assertEqual( result[0], result[1])
-		
+        self.assertEqual(result[0], result[1])
+
     def testCase9(self):
         result = TestBase()
-        self.assertEqual( result[0], result[1])
-		
+        self.assertEqual(result[0], result[1])
+
     def testCase10(self):
         result = TestBase()
-        self.assertEqual( result[0], result[1])
+        self.assertEqual(result[0], result[1])
+
 
 if __name__ == '__main__':
     unittest.main()
