1b58362ae9aa00ef2cfe25a4126cf8b80968062d
MamadEmin <komakkon2012@yahoo.com>
2017-11-30 13:37:37 -0800

    solve problem day 4

--- python practice/day4/solution.py
+++ python practice/day4/solution.py
@@ -1,31 +1,29 @@
 class Person:
-    personAge = 0
-    def __init__(self,initialAge):
-        # Add some more code to run some checks on initialAge
-        if initialAge < 0:
-            print("Age is not valid, setting age to 0.")
+    def __init__(self, initialAge):
+        if initialAge > 0:
+            self.age = initialAge
         else:
-            self.personAge = initialAge
-
+            print("Age is not valid, setting age to 0.")
+            self.age = 0
+#amIOld() should perform the following conditional actions:
     def amIOld(self):
-        # Do some computations in here and print out the correct statement to the console
-        if self.personAge < 13:
+        if self.age < 13:
             print("You are young.")
-        elif self.personAge < 18:
+        elif self.age < 18:
             print("You are a teenager.")
         else:
             print("You are old.")
 
     def yearPasses(self):
-        # Increment the age of the person in here
-        self.personAge += 1
-		
-t = int(input())
-for i in range(0, t):
-    age = int(input())         
-    p = Person(age)  
+        self.age += 1
+
+#T=Input Format,the number of test cases
+T = int(input())
+for i in range(0, T):
+    age = int(input())
+    p = Person(age)
     p.amIOld()
     for j in range(0, 3):
-        p.yearPasses()       
+        p.yearPasses()
     p.amIOld()
     print("")
