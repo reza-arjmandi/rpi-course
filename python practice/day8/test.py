acd17528d4550285837db056a3879d894484c96b
FSZolfaghari <z.fa95@yahoo.com>
2017-12-06 01:12:49 +0330

    test of problem 8

--- python practice/day8/test.py
+++ python practice/day8/test.py
@@ -3,55 +3,102 @@ from subprocess import Popen, PIPE, STDOUT
 import string
 import random
 
-def string_generator(size=6, chars=string.ascii_uppercase + string.digits + " " + string.punctuation):
-    return ''.join(random.choice(chars) for _ in range(size))
-		
+def stringMaker(size = 5, chars=string.ascii_uppercase):
+    return ''.join(random.choice(chars) for _ in range(size)) 
+
+def intMaker(min = 1 , max = 100000):
+    return random.randint(min , max)
+    
+def phoneMaker():
+    return str(random.randint(100,10000))
+
 def TestBase():
-    stdIn = string_generator()
-    p = Popen(['python', 'solution.py'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)   
+    N = intMaker()
+    ourIn = ''
+    ourIn = str(N)+"\r\n"
+    validQuery = []
+    phoneBook={}
+    for i in range(N):
+        name = stringMaker()
+        number = phoneMaker()
+        validQuery.insert(i,name)
+        ourIn += name + " " + number + "\r\n"
+        phoneBook[name]=number
+        
+    query = []
+    for i in range(len(validQuery)):
+        query.append(validQuery[i])
+        ourIn += validQuery[i] + "\r\n"
+        
+    for i in range(100000-len(validQuery)):
+        s = stringMaker()
+        query.append(s)
+        ourIn += s + "\r\n"
+        
+    ourOut = ''
+    for i in range(len(query)) :
+
+        if query[i] in phoneBook :
+            ourOut += query[i]+"="+phoneBook[query[i]]+"\r\n"
+        else:
+            ourOut += "Not found\r\n"
+            
+    stdIn = ourIn
+    p = Popen(['python', 'myPython.py'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)   
     stdout = p.communicate(input=bytes(stdIn, encoding='utf-8'))[0]
-    expectedStdout = "Hello word\r\n" + stdIn + "\r\n"
+
+    expectedStdout = ourOut
     return (stdout.decode(), expectedStdout)
 	
 class test(unittest.TestCase): 
-    
+
     def testCase1(self):
+        self.maxDiff=None
         result = TestBase()
         self.assertEqual( result[0], result[1])
 		
     def testCase2(self):
+        self.maxDiff=None
         result = TestBase()
         self.assertEqual( result[0], result[1])
 		
     def testCase3(self):
+        self.maxDiff=None
         result = TestBase()
         self.assertEqual( result[0], result[1])
 		
     def testCase4(self):
+        self.maxDiff=None
         result = TestBase()
         self.assertEqual( result[0], result[1])
 		
     def testCase5(self):
+        self.maxDiff=None
         result = TestBase()
         self.assertEqual( result[0], result[1])
 		
     def testCase6(self):
+        self.maxDiff=None
         result = TestBase()
         self.assertEqual( result[0], result[1])
 		
     def testCase7(self):
+        self.maxDiff=None
         result = TestBase()
         self.assertEqual( result[0], result[1])
 	
     def testCase8(self):
+        self.maxDiff=None
         result = TestBase()
         self.assertEqual( result[0], result[1])
 		
     def testCase9(self):
+        self.maxDiff=None
         result = TestBase()
         self.assertEqual( result[0], result[1])
 		
     def testCase10(self):
+        self.maxDiff=None
         result = TestBase()
         self.assertEqual( result[0], result[1])
 
