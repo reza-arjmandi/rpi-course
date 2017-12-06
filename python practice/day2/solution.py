f991af58d70938d9a92798b999d4b8bc2bd9d4c4
riyahiyan <h.riyahiyan@gmail.com>
2017-12-06 22:59:35 +0330

    day2 has been Solved

--- python practice/day2/solution.py
+++ python practice/day2/solution.py
@@ -1,10 +1,9 @@
-mealCost = float(input())
-tipPercent = int(input())
-taxPercent = int(input())
+mealCost=float(input())
+tipPercent=int(input())
+taxPercent=int(input())
 
-tip = (mealCost * tipPercent)/100
-tax = (mealCost * taxPercent)/100
-
-totalCost = mealCost + tax + tip
-
+tip = mealCost * (tipPercent / 100.0)
+tax = mealCost * (taxPercent / 100.0)
+totalCost = mealCost + tip + tax
+#'totalCost=totalCost+(mealCost * tipPercent / 100.0)+(mealCost * taxPercent / 100.0)
 print("The total meal cost is {} dollars.".format(round(totalCost)))
