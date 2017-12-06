c760a309cb3be1556b178ca37444f6f2f3da41a2
riyahiyan <h.riyahiyan@gmail.com>
2017-12-07 01:30:10 +0330

    day4 has been solved

--- python practice/day4/solution.py
+++ python practice/day4/solution.py
@@ -1,13 +1,26 @@
 class Person:
+    
     def __init__(self,initialAge):
         # Add some more code to run some checks on initialAge
+         self.i=initialAge        
+         if(self.i<0):
+            print("Age is not valid, setting age to 0.")
+            self.i=0
+                  
 
     def amIOld(self):
         # Do some computations in here and print out the correct statement to the console
+        if(self.i<13):
+            print("You are young.")
+        elif(self.i>=13 and self.i<18):
+            print("You are a teenager.")
+        else:
+            print("You are old.")
 
     def yearPasses(self):
-        # Increment the age of the person in here     
-		
+        # Increment the age of the person in here
+         self.i+=1
+        
 t = int(input())
 for i in range(0, t):
     age = int(input())         
