3d9e82071b93ac52c4a4b0c92e1345566d489ac0
baharehf <baharehfatemi@gmail.com>
2017-12-16 13:12:09 +0330

    Solve Problem Day13

--- python practice/day13/solution.py
+++ python practice/day13/solution.py
@@ -1,7 +1,26 @@
-import sys
+import os  # For using os.linesep instead of using a line separator: '\n'
 
-t = input()
+class Book(object):
+        # Initialize the book title, author(s) name and price
+	def __init__(self, title, author):
+                # Assign the value to class variables 
+		self.title = title
+		self.author = author
+   
+class MyBook(Book):
+        # Title: The book's title
+        # Author: author The book's author
+        # Price: The book's price
+	def __init__(self, title, author, price):
+		super().__init__(title, author)
+		self.price = price
 
-print("Hello word")
+	def display(self):
+                # Print the class variable values
+		print("Title:", self.title, os.linesep+"Author:", self.author, os.linesep+"Price:", self.price)
 
-print(t)
+title = input()
+author = input()
+price = int(input())
+new_novel = MyBook(title, author, price)
+new_novel.display()
