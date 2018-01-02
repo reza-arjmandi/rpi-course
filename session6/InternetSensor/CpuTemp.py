654e4c65b9698c02abc678dbcc14d3e69ee016a1
reza arjmandi <arjmandi.re@gmail.com>
2018-01-02 21:40:10 +0330

    add new IOT project

---
+++ session6/InternetSensor/CpuTemp.py
@@ -0,0 +1,17 @@
+######################################################################
+#	CpuTemp.py
+#
+# This program measure Raspberry pi cpu temprature and print it 
+# every one second
+######################################################################
+
+import os, time
+
+cmd = '/opt/vc/bin/vcgencmd measure_temp'
+
+while True:
+    dev = os.popen(cmd)
+    cpuTempStr = dev.read()[5:-3]
+    cpuTemp=float(cpuTempStr)
+    print(cpuTemp)
+    time.sleep(1)
