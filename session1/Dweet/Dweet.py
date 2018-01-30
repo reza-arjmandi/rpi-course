190cbd621bdb604f73b42999f179328e7894b54e
root <arjmandi.re@gmail.com>
2018-01-30 19:04:06 +0000

    add IOT project to session1

---
+++ session1/Dweet/Dweet.py
@@ -0,0 +1,39 @@
+######################################################################
+#       Dweet.py
+#
+# This project control 3 LEDs throughout the internet with
+# Dweet.io(IOT service).
+# You can see the client-side interface in Dweet.html or 
+# e-system.ir/iot/Dweet.html.
+######################################################################
+
+import time
+import dweepy
+import RPi.GPIO as GPIO
+
+KEY = 'rpi_course_iot_projects'
+
+led = [18, 23, 24]
+led_states = [0, 0, 0]
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+GPIO.setup(23, GPIO.OUT)
+GPIO.setup(24, GPIO.OUT)
+
+def UpdateLeds():
+    for i in range(len(led_states)):
+        GPIO.output(led[i], led_states[i])
+
+while True:
+    try:
+        for dweet in dweepy.listen_for_dweets_from(KEY):
+            if(dweet['content']['led0'] == 1):
+                led_states[0] = not led_states[0]
+            elif(dweet['content']['led1'] == 1):
+                led_states[1] = not led_states[1]
+            elif(dweet['content']['led2'] == 1):
+                led_states[2] = not led_states[2]
+            UpdateLeds()
+    except Exception as e:
+        pass
---
+++ session1/InternetSensor/InternetSensor.py
@@ -0,0 +1,20 @@
+######################################################################
+#	InternetSensor.py
+#
+# This program measure Raspberry pi cpu temprature and then send
+# it with a dweet.io service.
+######################################################################
+
+import os, time
+import urllib2
+
+def CpuTemp():
+    dev = os.popen('/opt/vc/bin/vcgencmd measure_temp')
+    temp = dev.read()[5:-3]
+    return temp
+
+while True:
+    temp = CpuTemp()
+    print("Raspberry Pi Temp: {}".format(temp))
+    urllib2.urlopen("https://dweet.io/dweet/for/RpiCourseDweet?temp={}".format(temp))
+    time.sleep(1)
