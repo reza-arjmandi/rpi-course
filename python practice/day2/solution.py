19c820e93580ef2d004097110afa2915a3f3799e
mo.javadian@gmail.com <mo.javadian@gmail.com>
2018-03-17 15:18:19 +0330

    solve problem day2

--- python practice/day2/solution.py
+++ python practice/day2/solution.py
@@ -1,8 +1,11 @@
 
+##mealCost = float(input("enter the cost of the meal before tax and tip: "))
+##tipPercent = int(input("enter the percentage mealCost of being added as tip: "))
+##taxPercent = int(input("the percentage mealCost of being added as tax: "))
+
 mealCost = float(input())
 tipPercent = int(input())
-taxPercent = int(input())
-
+taxPercent = int(input())   
 tip0 = mealCost * (tipPercent / 100.0)
 tax0 = mealCost * (taxPercent / 100.0)
 totalCost = mealCost + tip0 + tax0
