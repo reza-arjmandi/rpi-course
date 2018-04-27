7f422eaea130ef28370393c8539b53bb1d7a6c28
nmarandi <34411083+nmarandi@users.noreply.github.com>
2018-04-27 12:53:30 +0430

    test for day 17 problem

--- python practice/day17/test.py
+++ python practice/day17/test.py
@@ -3,15 +3,18 @@ from subprocess import Popen, PIPE, STDOUT
 import string
 import random
 
-def string_generator(size=6, chars=string.ascii_uppercase + string.digits + " " + string.punctuation):
-    return ''.join(random.choice(chars) for _ in range(size))
+def string_generator(size=1, chars=string.digits):
+    return ''.join(random.choice(chars) for _ in range(size)) + " "+''.join(random.choice(chars) for _ in range(size))
 		
 def TestBase():
     stdIn = string_generator()
-    p = Popen(['python', 'solution.py'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)   
+    p = Popen(['python', 'solution.py'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
     stdout = p.communicate(input=bytes(stdIn, encoding='utf-8'))[0]
-    expectedStdout = "Hello word\r\n" + stdIn + "\r\n"
-    return (stdout.decode(), expectedStdout)
+    expectedStdout = 1
+    for i in range(0,int(stdIn[2])):
+         expectedStdout = expectedStdout*int(stdIn[0])
+    return (stdout.decode(), str(expectedStdout)+"\r\n")
+
 	
 class test(unittest.TestCase): 
     
