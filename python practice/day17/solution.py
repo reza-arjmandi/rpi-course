178a41f330daa4ee5bb842507906422148035d29
nmarandi <34411083+nmarandi@users.noreply.github.com>
2018-04-27 11:35:01 +0430

    day 17

--- python practice/day17/solution.py
+++ python practice/day17/solution.py
@@ -1,7 +1,13 @@
 import sys
-
-t = input()
-
-print("Hello word")
-
-print(t)
+class Calculator:
+    def power(n,p):
+        if p < 0 or n < 0:
+            print('n and p should be non-negative')
+        z = 1 
+        for i in range(0,p):
+            z = z*n
+        return z;
+n,p = input().split()
+p = int(p)
+n = int(n)
+print(Calculator.power(n,p))
