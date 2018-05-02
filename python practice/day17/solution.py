2137f75610af7f45ee66e0a0f05ce145a3114ca9
arjmandi <arjmandi.re@gmail.com>
2018-05-02 17:48:56 +0430

    The solution corrected.

--- python practice/day17/solution.py
+++ python practice/day17/solution.py
@@ -1,14 +1,6 @@
 import sys
 class Calculator:
-    def power(n,p):
+    def power(self, n, p):
         if p < 0 or n < 0:
-            print('n and p should be non-negative')
-            raise ValueError('A very specific bad thing happened.')
-        z = 1 
-        for i in range(0,p):
-            z = z*n
-        return z;
-n,p = input().split()
-p = int(p)
-n = int(n)
-print(Calculator.power(n,p))
+            raise ValueError('n and p should be non-negative')
+        return n**p;
