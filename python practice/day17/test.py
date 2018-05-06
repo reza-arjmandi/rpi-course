d30fbed2b5f275edfb07de1f40113e522df5534f
nmarandi <34411083+nmarandi@users.noreply.github.com>
2018-05-06 21:21:51 +0430

    test.py refactor

--- python practice/day17/test.py
+++ python practice/day17/test.py
@@ -2,61 +2,58 @@ import unittest
 from subprocess import Popen, PIPE, STDOUT
 import string
 import random
+from solution import Calculator
 
 def string_generator(size=1, chars=string.digits):
     return ''.join(random.choice(chars) for _ in range(size)) + " "+''.join(random.choice(chars) for _ in range(size))
 		
-def TestBase():
-    stdIn = string_generator()
-    p = Popen(['python', 'solution.py'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
-    stdout = p.communicate(input=bytes(stdIn, encoding='utf-8'))[0]
-    expectedStdout = 1
-    for i in range(0,int(stdIn[2])):
-         expectedStdout = expectedStdout*int(stdIn[0])
-    return (stdout.decode(), str(expectedStdout)+"\r\n")
+def TestBase(n,p):
+    calc = Calculator()
+    pow = calc.power( n, p)
+    return pow
 
 	
 class test(unittest.TestCase): 
     
     def testCase1(self):
-        result = TestBase()
-        self.assertEqual( result[0], result[1])
+        result = TestBase(2,4)
+        self.assertEqual( 2**4, result)
 		
     def testCase2(self):
-        result = TestBase()
-        self.assertEqual( result[0], result[1])
+        result = TestBase(3,1)
+        self.assertEqual( 3**1, result)
 		
     def testCase3(self):
-        result = TestBase()
-        self.assertEqual( result[0], result[1])
+        result = TestBase(9,9)
+        self.assertEqual( 9**9, result)
 		
     def testCase4(self):
-        result = TestBase()
-        self.assertEqual( result[0], result[1])
+        result = TestBase(7,2)
+        self.assertEqual( 7**2, result)
 		
     def testCase5(self):
-        result = TestBase()
-        self.assertEqual( result[0], result[1])
+        result = TestBase(4,11)
+        self.assertEqual( 4**11, result)
 		
     def testCase6(self):
-        result = TestBase()
-        self.assertEqual( result[0], result[1])
+        result = TestBase(3,6)
+        self.assertEqual( 3**6, result)
 		
     def testCase7(self):
-        result = TestBase()
-        self.assertEqual( result[0], result[1])
+        result = TestBase(7,8)
+        self.assertEqual( 5764801, result)
 	
     def testCase8(self):
-        result = TestBase()
-        self.assertEqual( result[0], result[1])
+        result = TestBase(1,10)
+        self.assertEqual( 1, result)
 		
     def testCase9(self):
-        result = TestBase()
-        self.assertEqual( result[0], result[1])
+        result = TestBase(5,10)
+        self.assertEqual( 9765625, result)
 		
     def testCase10(self):
-        result = TestBase()
-        self.assertEqual( result[0], result[1])
+        result = TestBase(2,4)
+        self.assertEqual( 16, result)
 
 if __name__ == '__main__':
     unittest.main()
